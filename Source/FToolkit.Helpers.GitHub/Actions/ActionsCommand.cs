22c84e90c134f5a9d2e84ee4e28c3ef521ed365a
finphie <19237170+finphie@users.noreply.github.com>
2024-11-10 09:55:58 +0900

    プロジェクト追加 (#1)

---
+++ Source/FToolkit.Helpers.GitHub/Actions/ActionsCommand.cs
@@ -0,0 +1,32 @@
+﻿namespace FToolkit.Helpers.GitHub.Actions;
+
+/// <summary>
+/// GitHub Actions関連のコマンドです。
+/// </summary>
+public static class ActionsCommand
+{
+    /// <summary>
+    /// グループを開始します。
+    /// </summary>
+    /// <param name="title">タイトル</param>
+    /// <returns>グループを終了するための<see cref="IDisposable"/>オブジェクトを返します。</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="title"/>がnullです。</exception>
+    /// <exception cref="ArgumentException"><paramref name="title"/>が空です。</exception>
+    public static IDisposable BeginGroup(string title)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(title);
+        return new GitHubActionsGroupDisposable(title);
+    }
+
+    static void Group(string title) => Console.WriteLine($"::group::{title}");
+
+    static void EndGroup() => Console.WriteLine("::endgroup::");
+
+    struct GitHubActionsGroupDisposable : IDisposable
+    {
+        public GitHubActionsGroupDisposable(string title) => Group(title);
+
+        /// <inheritdoc/>
+        public readonly void Dispose() => EndGroup();
+    }
+}
---
+++ Source/FToolkit.Helpers.GitHub/RepositoryHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace FToolkit.Helpers.GitHub;
+
+/// <summary>
+/// GitHubリポジトリ関連のヘルパークラスです。
+/// </summary>
+public static class RepositoryHelper
+{
+    /// <summary>
+    /// オーナー名とリポジトリ名を取得します。
+    /// </summary>
+    /// <param name="repository">「オーナー名/リポジトリ名」形式の文字列。</param>
+    /// <returns>オーナー名とリポジトリ名を返します。</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="repository"/>がnullです。</exception>
+    /// <exception cref="ArgumentException"><paramref name="repository"/>が空か「オーナー名/リポジトリ名」形式ではありません。</exception>
+    public static (string Owner, string RepositoryName) GetRepositoryOwnerAndName(string repository)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(repository);
+
+        return repository.Split('/') is [{ Length: > 0 } repositoryOwner, { Length: > 0 } repositoryName]
+            ? (repositoryOwner, repositoryName)
+            : throw new ArgumentException("Invalid repository format. Expected format is owner/repo.");
+    }
+}
